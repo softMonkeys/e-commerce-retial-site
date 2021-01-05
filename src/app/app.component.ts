ac5a8a1deddaeb45ca1026d298ed470baaef8de0
Benjamin Chen <benkun.chen@gmail.com>
2021-01-04 18:43:41 -0800

    name change for the application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'e-commerce retial site';
   name = 'Ben';
   randomId = 45;
   randomStatus = 'offline';
